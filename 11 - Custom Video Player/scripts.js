e841b48345f8772bea2b43a248ca4103c159becb
Daniel Ahn <iamdanahn@gmail.com>
2021-05-11 20:29:25 -0400

    change play/pause button when video plays

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -10,14 +10,27 @@ const ranges = player.querySelectorAll(".player__slider");
 /* Functions */
 function togglePlay() {
   if (video.paused) {
-    video.play();
-  } else {
-    video.pause();
-  }
+		video.play(); // videos have
+	} else {
+		video.pause();
+	}
 
   // const method = video.paused ? "play" : "pause";
   // video[method]();
 }
 
+function updateButton() {
+  console.log('inside update button fn')
+
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+
+/* Event listeners */
 video.addEventListener('click', togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+
 toggle.addEventListener('click', togglePlay);
+
